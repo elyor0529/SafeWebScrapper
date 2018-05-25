a14b5b1c5d5b503d9d0e15415e8e30adf6d7127d
Elyor <elyor.blog@gmail.com>
2018-05-25 19:45:24 +0500

    clean

---
+++ edf-tools/EDF Modules/Jobboerse/ucExtSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DevExpress.XtraEditors;
+using WheelsScraper;
+
+namespace Databox.Libs.Jobboerse
+{
+    public partial class ucExtSettings : XtraUserControl
+    {
+        private ScraperSettings _sett;
+
+        public ucExtSettings()
+        {
+            InitializeComponent();
+        }
+
+        public ExtSettings ExtSett
+        {
+            get
+            {
+                return (ExtSettings)Sett.SpecialSettings;
+            }
+        }
+
+
+        public ScraperSettings Sett
+        {
+            get { return _sett; }
+            set
+            {
+                _sett = value;
+                if (_sett != null) RefreshBindings();
+            }
+        }
+
+        protected void RefreshBindings()
+        {
+             
+        }
+
+        private void ucExtSettings_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ edf-tools/EDF dev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EDF_dev
+{ 
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            //TODO: This is a dummy app for debugging EDF modules. Dont start it directly.
+        }
+    } 
+}
