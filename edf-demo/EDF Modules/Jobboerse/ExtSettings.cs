9ac238f1bb44e04cf6b7bf0787cd925cf04c9f61
Elyor <elyor.blog@gmail.com>
2018-05-26 17:29:42 +0500

    edf tools

---
+++ edf-demo/EDF Modules/Jobboerse/ExtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Databox.Libs.Jobboerse
+{
+	public class ExtSettings
+	{
+
+	    public string City { get; set; }
+
+        public string Keyword { get; set; }
+
+	}
+}
